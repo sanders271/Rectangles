c799777ce03b47770a0b273e2739222c6d055fcc
Aston Sanders <sande@DESKTOP-9ST8DA0.(none)>
2017-08-05 10:19:52 -0400

    Updated to be winforms app.

--- Rectangles/Rectangles/Properties/AssemblyInfo.cs
+++ Rectangles/Rectangles/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("106e87ce-7413-4480-9907-a8af8a5914e7")]
+[assembly: Guid("c17005ac-6305-44de-8bb2-1d5ed03c74b0")]
 
 // Version information for an assembly consists of the following four values:
 //
